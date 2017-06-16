468efe2d2789cd54d97892296e48d1e90c81bbf6
Rocho <rncastaniervivas@hotmail.com.ar>
2017-06-15 22:00:34 -0300

    clase de Criteria
    crear alias y simplificar consultas de sql

--- src/main/java/ar/edu/unlam/tallerweb1/dao/UsuarioDaoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/UsuarioDaoImpl.java
@@ -19,10 +19,18 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	public Usuario consultarUsuario(Usuario usuario) {
 
 		final Session session = sessionFactory.openSession();
-		return (Usuario) session.createCriteria(Usuario.class)
+		return (Usuario) session.createCriteria(Usuario.class) 
+				/*el parentesis de criteria devuelve el tipo de ese dato en este caso de tipo Usuario*/
 				.add(Restrictions.eq("email", usuario.getEmail()))
 				.add(Restrictions.eq("password", usuario.getPassword()))
 				.uniqueResult();
+				
+				/*el ComunaBuscada es el alias a generar independientemente del nombre en la tabla*/
+				/*.createAlias("Barrio.comuna", "ComunaBuscada");	
+				.add (Restriction.eq("ComunaBuscada.nombre","Comuna 10");
+				list();
+				agregamos un alias para buscar todas los barrios que pertenecen a la comuna 10
+				No ponemos .uniqueResult(); para que NO devuelva un solo resultado*/
 	}
 
 }
